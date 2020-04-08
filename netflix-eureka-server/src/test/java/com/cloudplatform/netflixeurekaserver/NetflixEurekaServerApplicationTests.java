184f2181784dcc3f624fecff125287a4bdce955c
“WuDG <1490727316@qq.com>
2020-04-08 18:19:25 +0800

    feat:初始化项目，添加eureka注册中心

---
+++ netflix-eureka-server/src/test/java/com/cloudplatform/netflixeurekaserver/NetflixEurekaServerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.cloudplatform.netflixeurekaserver;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+class NetflixEurekaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
