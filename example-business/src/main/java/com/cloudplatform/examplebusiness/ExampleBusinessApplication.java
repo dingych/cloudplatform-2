cdb5e8ba3d41b78c6b8509b611e706c0e67c1e4d
“WuDG <1490727316@qq.com>
2020-04-09 08:30:43 +0800

    feat:example-business微服务通过读取配置中心的数据库连接参数测试成功

--- example-business/src/main/java/com/cloudplatform/examplebusiness/ExampleBusinessApplication.java
+++ example-business/src/main/java/com/cloudplatform/examplebusiness/ExampleBusinessApplication.java
@@ -3,10 +3,13 @@ package com.cloudplatform.examplebusiness;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableDiscoveryClient
+
 @MapperScan("com.cloudplatform.examplebusiness.dao.*")
 public class ExampleBusinessApplication {
 
